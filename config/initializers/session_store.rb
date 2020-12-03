60d79e0cfc25464184e78f9ac2c718a0eb6dcbd8
Nick Mather <nickmather12@gmail.com>
2020-12-04 09:52:52 +1100

    domain set to all

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_strongr', domain: 'taboo-join.surge.sh'
+  Rails.application.config.session_store :cookie_store, key: '_strongr', domain: :all 
 else
   Rails.application.config.session_store :cookie_store, key: '_strongr'
 end
